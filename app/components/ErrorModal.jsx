39926c71e59d1ce7324dde76f9ede9a1eb1e654e
Attila Blascsák <colomortal@gmail.com>
2017-01-10 23:36:06 +0100

    Fix foundation modal error

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer =  require('react-dom/server');
 
 var ErrorModal = React.createClass({
   propTypes: {
@@ -11,22 +13,31 @@ var ErrorModal = React.createClass({
     }
   },
   componentDidMount: function () {
+    // Fix for foundation, so it can close the modal without using react
+    var {title, message} = this.props;
+
+    var modalMarkup = (
+      <div id="error-modal" className="tiny reveal text-center" data-reveal="">
+        <h4>{title}</h4>
+        <p>{message}</p>
+        <p>
+          <button className="hollow button" data-close="">Okay.</button>
+        </p>
+      </div>
+    );
     // Create a new Foundation Reveal instance, and set witch gets controlled
     // by it in the DOM
+
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
     var modal = new Foundation.Reveal($('#error-modal'));
     // Reveal the DOM element, when this component gets mounted
     modal.open();
   },
   render: function () {
-    var {title, message} = this.props;
-
     return (
-      <div id="error-modal" className="tiny reveal text-center" data-reveal="error-modal">
-        <h4>{title}</h4>
-        <p>{message}</p>
-        <p>
-          <button className="hollow button" data-close="error-modal">Okay.</button>
-        </p>
+      <div>
       </div>
     );
   }
