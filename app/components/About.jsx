b4029286e99a350bc5f4aa74683a02c026f98de4
Attila Blascsák <colomortal@gmail.com>
2017-01-10 20:27:36 +0100

    Updated About component with styling and a simple copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,14 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h4>About Component</h4>
+    <div>
+      <h1 className="text-centered">About</h1>
+      <p>This is a <a href="https://facebook.github.io/react/" alt="The official ReactJS page">ReactJS</a> learning project,
+       wich uses the <a href="https://openweathermap.org/" alt="The OpenWeatherMap Documentation">OpenWeatherMapApi</a> 
+       to fetch the weather, and the current temeature on the input location.</p>
+      <p>You can find the full source code on my <a href="https://github.com/blascsi/react-weather" 
+      alt="The repository of the project">GitHub profile!</a></p>
+    </div>
   );
 }
 
